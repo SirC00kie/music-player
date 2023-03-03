0b26d415ec135e7a11f87242b1acdd1d23f33446
SirC00kie <kostomarov.2001@mail.ru>
2023-03-03 12:01:47 +0300

    update player handler

--- internal/music-player/handlers/http/player.go
+++ internal/music-player/handlers/http/player.go
@@ -1,7 +1,7 @@
 package http
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"music-player/internal/music-player/models"
 	"music-player/internal/music-player/services"
@@ -18,9 +18,17 @@ func (ph *PlayerHandler) NextSong(w http.ResponseWriter, r *http.Request, _ http
 		http.Error(w, "No song is currently playing", http.StatusBadRequest)
 		return
 	}
+
 	song := ph.Service.Playlist.CurrentSong.Value.(*models.Song)
+	songJSON, err := json.Marshal(song)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("NextSong %s", song)))
+	w.Write(songJSON)
 }
 
 func (ph *PlayerHandler) PrevSong(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -29,9 +37,17 @@ func (ph *PlayerHandler) PrevSong(w http.ResponseWriter, r *http.Request, _ http
 		http.Error(w, "No song is currently playing", http.StatusBadRequest)
 		return
 	}
+
 	song := ph.Service.Playlist.CurrentSong.Value.(*models.Song)
+	songJSON, err := json.Marshal(song)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Prev song %s", song)))
+	w.Write(songJSON)
 }
 
 func (ph *PlayerHandler) PlaySong(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -40,9 +56,17 @@ func (ph *PlayerHandler) PlaySong(w http.ResponseWriter, r *http.Request, _ http
 		http.Error(w, "No song is currently playing", http.StatusBadRequest)
 		return
 	}
+
 	song := ph.Service.Playlist.CurrentSong.Value.(*models.Song)
+	songJSON, err := json.Marshal(song)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Playing song %s with duration %s", song.Title, song.Duration)))
+	w.Write(songJSON)
 }
 
 func (ph *PlayerHandler) PauseSong(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -51,7 +75,15 @@ func (ph *PlayerHandler) PauseSong(w http.ResponseWriter, r *http.Request, _ htt
 		http.Error(w, "No song is currently playing", http.StatusBadRequest)
 		return
 	}
+
 	song := ph.Service.Playlist.CurrentSong.Value.(*models.Song)
+	songJSON, err := json.Marshal(song)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Playing song %s pause time %s", song.Title, ph.Service.Playlist.PausedTime)))
+	w.Write(songJSON)
 }
